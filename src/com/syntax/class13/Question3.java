da8ade43a907f549be0154231ec57e794106fbde
Lilly Green <lillygreen@Lillys-MBP.lan1>
2020-03-25 23:35:31 -0500

    code for question3

---
+++ src/com/syntax/class13/Question3.java
@@ -0,0 +1,30 @@
+package com.syntax.class13;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Write a Java Program to print first 10 numbers of Fibonacci series.
+        //sum of previous numbers will give me fibonacci numbers
+		int a,b,c;
+		a=0;
+		b=1;
+	
+		for(int x=1; x<=10; x++) {
+			System.out.println(a+ " ");
+	    c=a+b;
+
+	    a=b;
+	    b=c;
+	    
+		
+		
+		
+		
+		
+		}
+
+	}
+
+}
