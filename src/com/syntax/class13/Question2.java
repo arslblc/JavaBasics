2c556eab8d1146a9d8169524aff5b8fb2a2c1849
Lilly Green <lillygreen@Lillys-MBP.lan1>
2020-03-25 23:09:06 -0500

    code for question2

--- src/com/syntax/class13/Question2.java
+++ src/com/syntax/class13/Question2.java
@@ -1,14 +1,29 @@
 package com.syntax.class13;
 
-import java.util.Scanner;
+
 
 public class Question2 {
 	public static void main (String [] args) {
 		//Write a java program to check whether a given number is prime or not?
+//divisible by1 and itself only
+		
 
-		Scanner scan=new Scanner (System.in);
-		int number=scan.nextInt();
-
+		int given=2;
+		boolean isPrime=true;
+		
+		if(given>1) {
+			 for(int x=2; x<given; x++) {
+				 if(given%x==0) {
+					 isPrime=false;
+					 break;
+				 } 
+			 }
+			
+		} else {
+			isPrime=false;
+		}
+		
+		System.out.println("Given number "+given+ " is prime ? "+isPrime );
 		
 		
 	}
